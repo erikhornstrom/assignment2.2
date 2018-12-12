a53ee73131afc11c451ea9885694a36f985706d9
erikhornstrom <erik.hornstrom@live.se>
2018-12-12 14:04:11 +0100

    added functions

--- bin/assignment2/2.clj
+++ bin/assignment2/2.clj
@@ -1,16 +1,31 @@
 (defmacro select [what _ list _ cond _ orderValue]
- 
+  `(sort-by ~orderValue (filterTable ~what ~list))
+  )
+
+(defmacro select2 [what _ list _ cond _ orderValue]
+  `(sort-by ~orderValue (filterTable ~what ~list))
   )
 
 
-(def persons `({:id 1 :name "olle"} {:id 2 :name "anna"} {:id 3 :name
+(def persons `({:id 1 :name "olle" :test 2} {:id 2 :name "anna"} {:id 3 :name
 "isak"} {:id 4 :name "beatrice"}))
 
-(select [:id :name] from persons where [:id > 2] orderby :name)
+;(select [:id :name] from persons where [:id > 2] orderby :name)
 
-({:id 4 :name "beatrice"} {:id 3 :name "isak"})
+(select2 [:id :name] from persons where [:id > 2] orderby :name)
 
-(print persons)
+
+(defn filterTable [what list]
+  (map (fn [n] (select-keys n what)) persons))
+
+(defn applyCondition [cond list]
+  (filter 
+    (fn [n] 
+      ( (second cond) (. n (first cond)) (last cond) ))
+  list)
+)
+
+
 
 ;göra map av map, skapa ny map med select-keys. applicera cond. sort-by
 ;1 map av map. 2 applicera cond. 3 select keys. Sort by.
